21f12b08dfa97f8e0669d7e3c8b1dc4e0c3e6512
Dylan Durdle <ddurdle@gmail.com>
2015-10-08 12:35:20 -0400

    debug

--- dicad.pl
+++ dicad.pl
@@ -79,8 +79,8 @@ if ($opt{I} ne ''){
 	  	}else{
     		print STDERR $duplicateMD5{$md5}[1] . "\n";
     		$duplicateMD5{$md5}[1] =~ s%\n%%;
-    		my $var = $opt{d};
-    		my ($path) = $duplicateMD5{$md5}[1] =~ m%$var\/(.*?)\/[^\/]+$%;
+    		my $var = quotemeta $opt{d};
+    		my ($path) = $duplicateMD5{$md5}[1] =~ m%\Q$var\/(.*?)\/[^\/]+$%;
     		print LOG 'mkdir -p "' .$opt{d}. '/'.$path . "\"\n";
     		print LOG 'cp "' . $duplicateMD5{$md5}[1] .'" "' .$opt{d}. "\"\n";
 	  	}
